63427d220668931b4dd945c06d05e374016cbeaf
Agustin Martinez <agussmartinez62@gmail.com>
2023-06-17 20:54:14 -0300

    reestructura

---
+++ HotelCabaniasMVC/HotelCabañas/Models/VMMantenimiento.cs
@@ -0,0 +1,32 @@
+﻿using LogicaNegocio.EntidadesNegocio;
+
+namespace HotelCabañas.Models
+{
+    public class VMMantenimiento
+    {
+        public int IdCabania { get; set; }
+        public DateTime Fecha1 { get; set; }
+        public DateTime Fecha2 { get; set; }
+
+        public Cabania Cabania { get; set; }
+        public IEnumerable<Mantenimiento> Mantenimientos { get; set; }
+
+
+        public VMMantenimiento() 
+        {
+            Fecha1 = DateTime.Now.AddMonths(-1);
+            Fecha2 = DateTime.Now;
+            this.Cabania = new Cabania();
+            this.Mantenimientos = new List<Mantenimiento>();
+        }
+
+        public VMMantenimiento(int idCabania, DateTime fecha1, DateTime fecha2, Cabania cabania)
+        {
+            IdCabania = idCabania;
+            Fecha1 = fecha1;
+            Fecha2 = fecha2;
+            Cabania = cabania;
+            this.Mantenimientos = new List<Mantenimiento>();
+        }
+    }
+}
---
+++ HotelCabaniasMVC/HotelCabañas/Models/VMTiposCabania.cs
@@ -0,0 +1,16 @@
+﻿using LogicaNegocio.EntidadesNegocio;
+
+namespace HotelCabañas.Models
+{
+    public class VMTiposCabania
+    {
+        public string StrSearch { get; set; }
+        public IEnumerable<TipoCabania> TiposCabania { get; set; }
+
+        public VMTiposCabania()
+        {
+            StrSearch = string.Empty;
+            TiposCabania = new List<TipoCabania>();
+        }
+    }
+}
